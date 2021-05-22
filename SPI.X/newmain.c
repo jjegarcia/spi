735aa30b74e089aad238ec1be39663d4cf384020
jjegarcia <jjegarcia@hotmail.com>
2021-05-22 18:36:38 +0100

    added_h

--- SPI.X/newmain.c
+++ SPI.X/newmain.c
@@ -12,21 +12,23 @@
 
 #define _XTAL_FREQ 8000000
 
-void __interrupt() service(){
+void __interrupt() service() {
     interruptService();
 }
+
 void main() {
     setIo();
     setInterrupts();
-    spiInit(SPI_MASTER_OSC_DIV4, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+    //    spiInit(SPI_MASTER_OSC_DIV4, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);//master
+    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE); //slave
 
     spiWrite(0b01010101);
     while (1) {
         if (FLAGS.ByteBits.DISPLAY_READING) {
             PORTD = readValue;
             FLAGS.ByteBits.DISPLAY_READING = 0;
-//            readValue = ~readValue;
-//            spiWrite(readValue);
+            //            readValue = ~readValue;
+            //            spiWrite(readValue);
         }
         __delay_ms(1000);
     }
