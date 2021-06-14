0698f7905f5eb7ca06cb5dc98456474890e7b552
jjegarcia <jjegarcia@hotmail.com>
2021-06-14 08:58:27 +0100

    successful loop-back test

--- SPI.X/newmain.c
+++ SPI.X/newmain.c
@@ -7,8 +7,8 @@
 #include "spi.h"
 #include "config.h"
 #include "main.h"
-#include "init.h"
 #include "interruptService.h"
+#include "init.h"
 
 #define _XTAL_FREQ 8000000
 
@@ -18,18 +18,29 @@ void __interrupt() service() {
 
 void main() {
     setIo();
+    setSPI();
+    setSerial();
     setInterrupts();
     //    spiInit(SPI_MASTER_OSC_DIV4, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);//master
-    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE); //slave
+//    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE); //slave
 
 //    spiWrite(0b01010101);
+    
+    TXREG= 0b11001100;
     while (1) {
-        if (FLAGS.ByteBits.DISPLAY_READING) {
-            PORTD = readValue;
-            FLAGS.ByteBits.DISPLAY_READING = 0;
-//                        readValue = ~readValue;
-                        spiWrite(0b00001111);
+        if (FLAGS.bits.DISPLAY_READING) {
+            //           PORTD = readSPIValue;
+            FLAGS.bits.DISPLAY_READING = 0;
+            //                        readValue = ~readValue;
+            spiWrite(0b00001111);
         }
-//        __delay_ms(1000);
+        if (FLAGS.bits.UART_RECEIVED) {
+            readSerialValue = RCREG;
+            PORTD = readSerialValue;
+            TXREG = readSerialValue;
+        }
+
+        //        __delay_ms(1000);
     }
 }
+
